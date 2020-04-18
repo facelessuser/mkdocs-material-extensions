d6faba6e448f8d5f2d2400138a5bcbe14d601db7
facelessuser <faceless.shop@gmail.com>
2020-04-18 11:53:45 -0600

    Add support for custom icons
    Closes #5

--- materialx/emoji.py
+++ materialx/emoji.py
@@ -9,38 +9,50 @@ import copy
 import codecs
 import inspect
 import material
+import pymdownx
 from pymdownx.emoji import TWEMOJI_SVG_CDN, add_attriubtes
 import xml.etree.ElementTree as etree  # noqa: N813
 
+NO_DEEP_COPY = pymdownx.__version_info__ >= (7, 1, 0)
 RESOURCES = os.path.dirname(inspect.getfile(material))
 
 
-def _patch_index(index):
+def _patch_index(index, options):
     """Patch the given index."""
 
+    icon_locations = options.get('custom_icons', [])
+    icon_locations.append(os.path.join(RESOURCES, '.icons'))
+
     # Find our icons
-    icon_path = os.path.join(RESOURCES, '.icons')
-    norm_base = icon_path.replace('\\', '/') + '/'
-    for result in glob.glob(icon_path.replace('\\', '/') + '/**/*.svg', recursive=True):
-        name = ':{}:'.format(result.replace('\\', '/').replace(norm_base, '', 1).replace('/', '-').lstrip('.')[:-4])
-        if name not in index['emoji'] and name not in index['aliases']:
-            # Easiest to just store the path and pull it out from the index
-            index["emoji"][name] = {'name': name, 'path': result}
+    for icon_path in icon_locations:
+        norm_base = icon_path.replace('\\', '/') + '/'
+        for result in glob.glob(icon_path.replace('\\', '/') + '/**/*.svg', recursive=True):
+            name = ':{}:'.format(result.replace('\\', '/').replace(norm_base, '', 1).replace('/', '-').lstrip('.')[:-4])
+            if name not in index['emoji'] and name not in index['aliases']:
+                # Easiest to just store the path and pull it out from the index
+                index["emoji"][name] = {'name': name, 'path': result}
 
 
-def twemoji():
+def twemoji(*args):
     """Provide a copied Twemoji index with additional codes for Material included icons."""
 
     import pymdownx.twemoji_db as twemoji_db
 
+    options = {}
+    md = None
+
+    if args:
+        options = args[1]
+        md = args[2]
+
     # Copy the Twemoji index
     index = {
         "name": 'twemoji',
-        "emoji": copy.deepcopy(twemoji_db.emoji),
-        "aliases": copy.deepcopy(twemoji_db.aliases)
+        "emoji": copy.deepcopy(twemoji_db.emoji) if not NO_DEEP_COPY else twemoji_db.emoji,
+        "aliases": copy.deepcopy(twemoji_db.aliases) if not NO_DEEP_COPY else twemoji_db.aliases
     }
 
-    _patch_index(index)
+    _patch_index(index, options)
 
     return index
 
